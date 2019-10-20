d7321c5e81a8815f52fa9ff067c972b582d3a141
Salah Adawi <hahli144@hotmail.com>
2019-10-20 18:49:16 +0300

    create file ft_memalloc.c

---
+++ ft_memalloc.c
@@ -0,0 +1,20 @@
+//ADD HEADER
+
+#include <stdlib.h> //CHANGE TO "libft.h"
+#include <string.h> //
+
+void	*ft_memalloc(size_t size)
+{
+  unsigned char *ptr;
+  int i;
+  
+  if (!(ptr = (unsigned char*)malloc(size)))
+    return (NULL);
+  i = 0;
+  while (i < size)
+    {
+      ptr[i] = 'a';
+      i++;
+    }
+  return (ptr);
+}
