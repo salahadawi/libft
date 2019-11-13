38dd9b9adeb42fca74df3a1ee5cfd6d352896878
Salah Adawi <hahli144@hotmail.com>
2019-11-13 13:41:01 +0200

    change itoa to use ft_nbrlen

--- ft_itoa.c
+++ ft_itoa.c
@@ -13,21 +13,6 @@
 #include "libft.h"
 #include <stdlib.h>
 
-static	int	ft_nbrlen(int nbr)
-{
-	int i;
-
-	i = 0;
-	if (nbr < 0)
-		i++;
-	while (nbr || i == 0)
-	{
-		nbr /= 10;
-		i++;
-	}
-	return (i);
-}
-
 char		*ft_itoa(int n)
 {
 	int		i;
