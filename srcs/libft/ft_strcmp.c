1adb24ddddfbe7f85b4179a7502856fdf2983646
sadawi <hahli144@hotmail.com>
2020-03-28 20:59:08 +0200

    fix seg fault with null input

--- srcs/libft/ft_strcmp.c
+++ srcs/libft/ft_strcmp.c
@@ -6,15 +6,21 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 14:57:01 by sadawi            #+#    #+#             */
-/*   Updated: 2019/10/23 16:41:25 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/03/24 20:07:18 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	int i;
 
 	i = 0;
+	if (!s1 && !s2)
+		return (0);
+	if ((s1 && !s2) || (!s1 && s2))
+		return (1);
 	while (s1[i] || s2[i])
 	{
 		if ((unsigned char)s1[i] != (unsigned char)s2[i])
